b7125eb74b66d1198a46c6ebf74505353a683eae
tanvikamble009-lab <tanvikamble009@gmail.com>
2025-11-15 23:02:13 +0530

    Update Prac8.c

--- 8/Prac8.c
+++ 8/Prac8.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct Edge {
     int src, dest, weight;
 };
 
-
 struct Subset {
     int parent, rank;
 };
-
 
 int find(struct Subset subsets[], int i) {
     if (subsets[i].parent != i)
@@ -18,85 +15,93 @@ int find(struct Subset subsets[], int i) {
     return subsets[i].parent;
 }
 
-
 void Union(struct Subset subsets[], int x, int y) {
-    int xroot = find(subsets, x);
-    int yroot = find(subsets, y);
+    int xr = find(subsets, x);
+    int yr = find(subsets, y);
 
-    if (subsets[xroot].rank < subsets[yroot].rank)
-        subsets[xroot].parent = yroot;
-    else if (subsets[xroot].rank > subsets[yroot].rank)
-        subsets[yroot].parent = xroot;
+    if (subsets[xr].rank < subsets[yr].rank)
+        subsets[xr].parent = yr;
+    else if (subsets[xr].rank > subsets[yr].rank)
+        subsets[yr].parent = xr;
     else {
-        subsets[yroot].parent = xroot;
-        subsets[xroot].rank++;
+        subsets[yr].parent = xr;
+        subsets[xr].rank++;
     }
 }
 
-
 int compareEdges(const void* a, const void* b) {
-    struct Edge* a1 = (struct Edge*)a;
-    struct Edge* b1 = (struct Edge*)b;
-    return a1->weight - b1->weight;
+    return ((struct Edge*)a)->weight - ((struct Edge*)b)->weight;
 }
 
-
 void KruskalMST(struct Edge edges[], int V, int E) {
-    struct Edge result[V]; 
-    int e = 0; 
-    int i = 0;
-    
+
+    int total = 0;   
+    int e = 0, i = 0;
+
     qsort(edges, E, sizeof(struct Edge), compareEdges);
 
-
-    struct Subset* subsets = (struct Subset*)malloc(V * sizeof(struct Subset));
-
-
-    int v;
-    for (v = 0; v < V; ++v) {
+    struct Subset* subsets = malloc(V * sizeof(struct Subset));
+    for (int v = 0; v < V; v++) {
         subsets[v].parent = v;
         subsets[v].rank = 0;
     }
 
+    printf("\nMST:\n");
 
     while (e < V - 1 && i < E) {
-        
-        struct Edge next_edge = edges[i++];
+        struct Edge next = edges[i++];
+        int x = find(subsets, next.src);
+        int y = find(subsets, next.dest);
 
-        int x = find(subsets, next_edge.src);
-        int y = find(subsets, next_edge.dest);
-
-        
         if (x != y) {
-            result[e++] = next_edge;
+            printf("Edge selected: %d-%d\n", next.src + 1, next.dest + 1);
+            total += next.weight;
+            e++;
             Union(subsets, x, y);
         }
-        
     }
-    printf("\nEdges in the Minimum Spanning Tree (MST):\n");
-    int totalWeight = 0;
-    for (i = 0; i < e; ++i) {
-        printf("%d -- %d == %d\n", result[i].src, result[i].dest, result[i].weight);
-        totalWeight += result[i].weight;
-    }
-    printf("Total weight of MST: %d\n", totalWeight);
+
+    printf("\nTotal Cost of MST = %d\n", total);
+    printf("\nMST Completed.\n");
 }
 
-
 int main() {
-    int V, E;
-    printf("Enter number of vertices and edges: ");
-    scanf("%d %d", &V, &E);
 
-    struct Edge* edges = (struct Edge*)malloc(E * sizeof(struct Edge));
+    int V;
+    printf("Enter number of vertices: ");
+    fflush(stdout);
+    scanf("%d", &V);
 
-    printf("Enter edges (source destination weight):\n");
-    int i;
-    for (i = 0; i < E; i++) {
-        scanf("%d %d %d", &edges[i].src, &edges[i].dest, &edges[i].weight);
+    int matrix[V][V];
+    int E = 0;
+
+    printf("Enter adjacency matrix:\n");
+    fflush(stdout);
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            scanf("%d", &matrix[i][j]);
+            if (matrix[i][j] != 0 && i < j)
+                E++;
+        }
+    }
+
+    struct Edge* edges = malloc(E * sizeof(struct Edge));
+    int idx = 0;
+
+    for (int i = 0; i < V; i++) {
+        for (int j = i + 1; j < V; j++) {
+            if (matrix[i][j] != 0) {
+                edges[idx].src = i;
+                edges[idx].dest = j;
+                edges[idx].weight = matrix[i][j];
+                idx++;
+            }
+        }
     }
 
     KruskalMST(edges, V, E);
 
     return 0;
 }
+
